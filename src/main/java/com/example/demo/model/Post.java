3a9574573883c6338ac11a5bf53e93db67ab71bf
Anatoliy <magic-t@bk.ru>
2024-02-19 05:56:28 +0500

    2.6 Bootstrap

--- src/main/java/com/example/demo/model/Post.java
+++ src/main/java/com/example/demo/model/Post.java
@@ -1,11 +1,15 @@
 package com.example.demo.model;
 
+import java.util.Date;
+
 public class Post {
     private String text;
     private Integer likes = 0;
+    private Date creationDate;
 
-    public Post(String text){
+    public Post(String text, Date creationDate){
         this.text = text;
+        this.creationDate = creationDate;
     }
 
     public String getText(){
@@ -15,4 +19,6 @@ public class Post {
     public Integer getLikes(){
         return likes;
     }
+
+    public Date getCreationDate(){ return creationDate; }
 }
--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -3,15 +3,18 @@ package com.example.demo.service;
 import com.example.demo.model.Post;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.DateFormatter;
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class PostService {
+
     public List<Post> listAllPosts(){
-        return Arrays.asList(new Post("Hello,"),
-                new Post("World"),
-                new Post(",!"));
+        return Arrays.asList(new Post("Hello,", new Date() ),
+                new Post("World", new Date()),
+                new Post(",!", new Date()));
     }
 }
